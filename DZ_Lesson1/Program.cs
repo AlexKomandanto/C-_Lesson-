faa4ae91730e6be84b0ec502442b19aa3c13ff03
Алексей Юрьевич <107716679+AlexKomandanto@users.noreply.github.com>
2022-07-31 16:11:56 +0300

    add DZ Lesson 1

---
+++ DZ_Lesson1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("input numberA: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("input numberB: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+    Console.Write(" a= " + numberA + "; b= " + numberB + " -> " + " max " + numberA);
+else
+    Console.Write(" a= " + numberA + ",; b= " + numberB + " -> " + " max " + numberB);
+
+    
+// a = 5; b = 7 -> max = 7
