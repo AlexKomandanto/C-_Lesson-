e305e62ec74b7663c0401ad4d413876f81efaa35
Алексей Юрьевич <107716679+AlexKomandanto@users.noreply.github.com>
2022-08-02 23:03:14 +0300

    add Example 008 Array, Method

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,17 +1,82 @@
-﻿int [] arrray = {1, 12,31, 4, 15,16,17,19 }; 
+﻿// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2 ;
+//     if (arg3 > result) result = arg3 ;
+//     return result;
+// }
 
-int n = arrray.Length;
-int find = 4;
+// int [] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
 
-int index = 0;
-while (index < n)
+// int max = Max (
+//     Max(array[0], array[1], array[2]),
+//     Max(array[3], array[4], array[5]),
+//     Max(array[6], array[7], array[8])
+// );
+// Console.WriteLine(max);
+
+
+// int [] array = {1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+// int n = array.Length;
+// int find = 18;
+// int index = 0; 
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index ++;
+// }
+
+void FillArray(int[] collection)
 {
-
-    if(arrray[index] == find)
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
     {
-        Console.WriteLine(index);
+        collection[index] = new Random().Next(1, 10);
+        index++;
     }
-    // index = index + 1
-   index++; 
 }
 
+void PrintArray(int[] col)
+
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
+    
